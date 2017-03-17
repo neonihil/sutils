6b95de8710c5c7358cd370b8111c0d59fb51d93c
Daniel Kovacs <dana@deasys.net>
2017-03-17 14:29:21 +1100

    Added PackageInfo class to handle general package information

---
+++ src/sutils/packageinfo.py
@@ -0,0 +1,53 @@
+# encoding: utf-8
+# name: libfirebase
+# version: @1.0
+# author: Daniel Kovacs <dkovacs@firstpixel.com.au>
+# file-name: gae.py
+# file-description: Google App Engine helper classes
+# file-version: 1.0
+# @@{PACKAGE_LICENSE_HEADER}
+#  
+#
+
+# -------------------------------------------------------------------------------
+# imports
+# -------------------------------------------------------------------------------
+
+import os
+import yaml
+from .primitives import qdict, qlist
+
+
+# -------------------------------------------------------------------------------
+# exports
+# -------------------------------------------------------------------------------
+
+__all__ = qlist()
+
+
+# -------------------------------------------------------------------------------
+# cachedkeyproperty
+# -------------------------------------------------------------------------------
+
+@__all__.register
+class PackageInfo(qdict):
+
+    def __init__(self, package_home, name = None, description = None, version = None, package_info_path = None, debug = True ):
+        self.package_home = package_home
+        self.name = name
+        self.description = description
+        self.version = version
+        self.debug = debug
+        self.package_info_path = package_info_path or os.path.join(self.package_home, 'package.yaml')
+        if (os.path.isfile(self.package_info_path)):
+            self.load_package_info()
+
+    def load_package_info(self, path = None):
+        path = path or self.package_info_path
+        info = yaml.load(open(path, 'r').read())
+        tier = info.get('tier', 'dev')
+        tier_info = info.pop("tiers", None)
+        self.update(info, recursive = True, add_keys = True, convert_to_qdict = True)        
+        if tier_info and tier_info.get(tier,None):
+            self.update(tier_info[tier], recursive = True, add_keys = True, convert_to_qdict = True)
+
