996ca2d383844486efcdcfc44b50a7d9f0faf2e9
Daniel Kovacs <dkovacs@deasys.net>
2019-01-29 16:07:26 +1100

    Bump version: 0.6.0 → 0.7.0

--- src/sutils/__init__.py
+++ src/sutils/__init__.py
@@ -24,7 +24,7 @@ __email__ = "dana@deasys.net"
 __description__ = "Smart Utilities"
 __license__ = "GPLv3+"
 __uri__ = "https://github.com/ultralightweight/sutils"
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 from .primitives import *
 from .logging_utils import *
